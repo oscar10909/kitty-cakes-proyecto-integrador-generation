99727553f1d8e001be06296dba685029c9862c6a
oscar10909 <72288043+oscar10909@users.noreply.github.com>
2022-02-24 17:00:10 +0000

    Deploying to gh-pages from @ oscar10909/kitty-cakes-proyecto-integrador-generation@2275ff995303e168451e8726691be61424e28cce 🚀

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,3 +1,7 @@
+
+/* Aolicacion de loader 
+a cada una de las paginas con una clase en cada
+una de las paginas */
 window.onload=function(){
     /* alert("cargando...") */
      $('#onload').fadeOut(); 
